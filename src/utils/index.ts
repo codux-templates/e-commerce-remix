8fce30806b685086045aea6d3e5e11201478d8fd
Yurii Venher <yuriiv@wix.com>
2024-09-16 10:20:25 +0300

    Use toError

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,26 @@ export function getUrlOriginWithPath(url: string) {
     const { origin, pathname } = new URL(url);
     return new URL(pathname, origin).toString();
 }
+
+export function toError(value: unknown): Error {
+    if (value instanceof Error) {
+        return value;
+    }
+
+    if (typeof value === 'undefined') {
+        return new Error();
+    }
+
+    let errorMessage = String(value);
+    if (typeof value === 'object' && value !== null) {
+        if ('message' in value) {
+            errorMessage = String(value.message);
+        }
+
+        if ('data' in value) {
+            errorMessage = String(value.data);
+        }
+    }
+
+    return new Error(errorMessage);
+}
