5c04eaf1ef698ddf2d94066936dd2c44bfbb23da
Oleh Rak <olehr@wix.com>
2024-09-20 14:32:27 +0300

    fix comment

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -8,9 +8,11 @@ export function getUrlOriginWithPath(url: string) {
 
 /*
  * Retrieves the message from a thrown error.
- * - Handles Remix ErrorResponse (non-Error instances).
- * - Handles Wix eCom SDK errors (non-Error instances).
- * - Defaults to converting the value to a string if no specific error type is matched.
+ * - Handles Remix ErrorResponse (non-Error instance).
+ * - Handles Wix eCom SDK errors (non-Error instance).
+ * - Converts plain objects to a JSON string as a measure
+ *   to help identify the origin of such improper errors.
+ * - Falls back on converting the value to a string.
  */
 export function getErrorMessage(value: unknown): string {
     if (value instanceof Error) {
